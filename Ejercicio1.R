be785e895db8be626fc0d0c2a30f670b5d29cb5f
sofiaCorral <71374206+sofiaCorral@users.noreply.github.com>
2020-11-25 21:24:23 +0100

    Modificación 25/11/20

--- Ejercicio1.R
+++ Ejercicio1.R
@@ -1,18 +1,25 @@
+
+# EJERCICIO 1 -------------------------------------------------------------
+
 #SOFIA CORRAL CABALLERO
 #21846911
 
 #ENUNCIADO DEL EJERCICIO####
-#Queremos leer un archivo el cual los generaremos en
+#Queremos leer un archivo el cual lo generaremos en
 #tiempo real, procesaremos el archivo y obtendremos
 #información útil 
 
 #CARGAMOS EL DATASET####
-#setwd('C:\\Users\\sofia\\OneDrive\\Documentos\\UEM1\\Tercer año\\LPE\\EJERCICIOS\\EJERCICIO 1 (Cargar dataset tiempo real)')
+#Lo siguiente es una opcion para cargar el dataset pero es recomendable hacerlo con read_csv y el url
 dataSet <- read.csv(file.choose(),header = TRUE, sep = ',')
 print(dataSet)
 length(dataSet)
 
+
+
 #RENOMBRAMOS LAS COLUMNAS####
+#Renombrar las columnas esta bien cuando hablamos de un dataset pequeño
+#Ademas, hacemos que este programa solo funcione para este dataset concreto
 names (dataSet) = c("Fecha", "Correo", "MiDescripcion", "Hyflex", "Nombre", "Mes", "Year",
                            "Apellido", "Descripcion_compañero", "EsRepetidor", "Expediente", "GitHub", "NivelProgramacion",
                            "PresentadoExpo", "DigitalBlock")
@@ -22,23 +29,24 @@ names (dataSet) = c("Fecha", "Correo", "MiDescripcion", "Hyflex", "Nombre", "Mes
    
 
 #1 ¿CUAL ES EL ALUMNO MÁS VIEJO?####
-attach(dataSet)
+attach(dataSet) #convertirlo en buscarlo
 min(Year)
 which.min(Year)
+subset(dataSet, Year == min(Year))#En este caso me imprime toda la fila
 
-alumnoViejo <- subset(dataSet, Year == min(Year), "Nombre")
+alumnoViejo <- subset(dataSet, Year == min(Year), "Nombre")#solo imprime el nombre
 alumnoViejo
 
 #2 ¿Cuantos alumnos cumplen años este mes?####
-
+#No puedo poner a mano el numero del mes, uso 'today'
 cumpleanios <- sum(dataSet$Mes == "11")
 cumpleanios
 
 #3 Listado de los alumnos con expediente par e impar####
-pares <- subset(dataSet, Expediente%%2 == 0, "Nombre")
+pares <- subset(dataSet, Expediente%%2 == 0, "Nombre")#solo imprime el nombre
 pares
 
-impares <- subset(dataSet, Expediente%%2 != 0, "Nombre")
+impares <- subset(dataSet, Expediente%%2 != 0, "Nombre")#solo imprime el nombre
 impares
 
 #4 ¿Cual es la palabra mas usada ?, ¿Que significado ?####
@@ -59,13 +67,14 @@ NuestraDescripcion <- readLines(MiDescripcion)
  
 
 #5 Porcentaje de alumnos que hicieron la expo####
+#Hay una opcion mejor sin usar el percent, con el count y calculos
 AlumnosSiExpo <- subset(dataSet, PresentadoExpo == 0, "Nombre")
 AlumnosSiExpo
 sum(dataSet$PresentadoExpo == 0)
 percent(sum(dataSet$PresentadoExpo == 0)/length(PresentadoExpo))  
 
 #6 Numero y listado de asistentes a clase tanto Hyflex como presenciales####
-alumnosPresencial <- subset(dataSet, Hyflex == 0, "Nombre")
+alumnosPresencial <- subset(dataSet, Hyflex == 0, "Nombre")#solo imprime el nombre
 alumnosPresencial
 
 sumaPresencial <- sum(dataSet$Hyflex == 0)
